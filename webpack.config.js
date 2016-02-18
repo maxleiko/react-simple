85b3559f7e1b181ba207e7644fee2d519c55dbd9
Maxime Tricoire <max.tricoire@gmail.com>
2016-02-18 16:01:42 +0100

    improvement

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
       }
-    }]
+    ]
   }
 };
